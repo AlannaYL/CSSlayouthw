22db4957eb438d30bf80d10de623d99ddee64272
Alanna <a04810254@gmail.com>
2022-12-27 16:32:52 +0800

    修好輪播圖，新增電腦切版

--- swiper.js
+++ swiper.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper('.swiper', {
+const swiper1 = new Swiper('#swiper1', {
   // Optional parameters
   direction: 'horizontal',
   loop: true,
@@ -8,4 +8,58 @@ const swiper = new Swiper('.swiper', {
     el: '.swiper-pagination',
     clickable: true,
   }
+});
+
+const swiper2 = new Swiper('#Latest-Blog', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 3,
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 3
+    },
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+const swiper3 = new Swiper('#swiper3', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 2,
+  spaceBetween: 4,
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 4
+    },
+    991: {
+      slidesPerView: 3,
+      spaceBetween: 4
+    },
+    1080: {
+      slidesPerView: 4,
+      spaceBetween: 4
+    },
+    1200: {
+      slidesPerView: 5,
+      spaceBetween: 4
+    },
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 });
